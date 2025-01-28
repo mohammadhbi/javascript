d9fe46033c82d712eaba66c169e4046e385047e6
mohammad <mohammad19hobbi@gmail.com>
2025-01-28 17:33:57 +0330

    hour

---
+++ clock.js
@@ -0,0 +1,13 @@
+function is12hour(hour){
+    if(hour==0){
+        console.log('12')
+    } else if( hour <=12){
+        console.log( hour)
+    }else if(hour >13 && hour<24){
+        console.log((hour-12 ))
+    }else{
+        console.log("edit your input")
+    }
+}
+console.log(is12hour(23))
+
